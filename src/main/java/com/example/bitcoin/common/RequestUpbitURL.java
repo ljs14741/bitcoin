c9b739faa726b7675d92f8dd770bddc8e03cd46e
binary <ljs14741@naver.com>
2024-05-01 13:41:41 +0900

    매수하기 , 매도하기 기능 추가

--- src/main/java/com/example/bitcoin/common/RequestUpbitURL.java
+++ src/main/java/com/example/bitcoin/common/RequestUpbitURL.java
@@ -1,10 +1,32 @@
 package com.example.bitcoin.common;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.HttpResponse;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @Slf4j
 @Component
@@ -30,5 +52,58 @@ public class RequestUpbitURL {
 
         return data;
 
+    }
+
+    public void executeTrade(String market, String side, String volume, String price, String ord_type) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        String accessKey = System.getenv("UPBIT_OPEN_API_ACCESS_KEY");
+        String secretKey = System.getenv("UPBIT_OPEN_API_SECRET_KEY");
+        String serverUrl = System.getenv("UPBIT_OPEN_API_SERVER_URL");
+
+        HashMap<String, String> params = new HashMap<>();
+        params.put("market", market);   /* 마켓ID (종목) (필수) */
+        params.put("side", side);         /* 주문종류 (필수) bid: 매수, ask: 매도 */
+        params.put("volume", volume);    /* 주문량 (지정가,시장가 매도 시 필수) */
+        params.put("price", price);       /* 주문가격 */
+        params.put("ord_type", ord_type);   /* 주문타입 (필수) limit: 지정가, price: 시장가(매수), market(매도) */
+
+        ArrayList<String> queryElements = new ArrayList<>();
+        for(Map.Entry<String, String> entity : params.entrySet()) {
+            queryElements.add(entity.getKey() + "=" + entity.getValue());
+        }
+
+        String queryString = String.join("&", queryElements.toArray(new String[0]));
+
+        MessageDigest md = MessageDigest.getInstance("SHA-512");
+        md.update(queryString.getBytes("UTF-8"));
+
+        String queryHash = String.format("%0128x", new BigInteger(1, md.digest()));
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        String jwtToken = JWT.create()
+                .withClaim("access_key", accessKey)
+                .withClaim("nonce", UUID.randomUUID().toString())
+                .withClaim("query_hash", queryHash)
+                .withClaim("query_hash_alg", "SHA512")
+                .sign(algorithm);
+
+        String authenticationToken = "Bearer " + jwtToken;
+
+        try {
+            CloseableHttpClient client = HttpClientBuilder.create().build();
+            HttpPost request = new HttpPost(serverUrl + "/v1/orders");
+            request.setHeader("Content-Type", "application/json");
+            request.addHeader("Authorization", authenticationToken);
+            request.setEntity(new StringEntity(new Gson().toJson(params)));
+
+            HttpResponse response = client.execute(request);
+            HttpEntity entity = ((CloseableHttpResponse) response).getEntity();
+
+            System.out.println();
+            log.info("RequestOrder: " + EntityUtils.toString(entity, "UTF-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
