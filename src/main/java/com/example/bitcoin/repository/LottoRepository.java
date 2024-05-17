d2bd6c7b7955c2f82fc6cf53e6585d531d98ef64
binary <ljs14741@naver.com>
2024-05-17 21:48:04 +0900

    화면 복채보내기 추가 및 테이블 오타 수정

--- src/main/java/com/example/bitcoin/repository/LottoRepository.java
+++ src/main/java/com/example/bitcoin/repository/LottoRepository.java
@@ -12,12 +12,12 @@ public interface LottoRepository extends JpaRepository<Lotto, Long> {
     List<Lotto> findByOrderByRoundNumberDesc();
 
     @Query(value = "SELECT number, COUNT(number) AS count FROM (" +
-            "SELECT l.number1 AS number FROM Lotto l UNION ALL " +
-            "SELECT l.number2 FROM Lotto l UNION ALL " +
-            "SELECT l.number3 FROM Lotto l UNION ALL " +
-            "SELECT l.number4 FROM Lotto l UNION ALL " +
-            "SELECT l.number5 FROM Lotto l UNION ALL " +
-            "SELECT l.number6 FROM Lotto l) AS allNumbers " +
+            "SELECT l.number1 AS number FROM lotto l UNION ALL " +
+            "SELECT l.number2 FROM lotto l UNION ALL " +
+            "SELECT l.number3 FROM lotto l UNION ALL " +
+            "SELECT l.number4 FROM lotto l UNION ALL " +
+            "SELECT l.number5 FROM lotto l UNION ALL " +
+            "SELECT l.number6 FROM lotto l) AS allNumbers " +
             "GROUP BY number ORDER BY count DESC", nativeQuery = true)
     List<Object[]> findNumberFrequencies();
 }
