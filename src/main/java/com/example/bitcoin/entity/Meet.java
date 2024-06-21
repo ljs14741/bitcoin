fa11b3e2a975d97805b6888b744535a593baac78
binary <ljs14741@naver.com>
2024-06-21 23:26:12 +0900

    익명 투표 모임 개설 기능 추가중

---
+++ src/main/java/com/example/bitcoin/entity/Meet.java
@@ -0,0 +1,40 @@
+package com.example.bitcoin.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter //클래스의 포함된 멤버 변수의 모든 getter 매서드를 생성
+@Setter
+@Builder // sql에 값 넣는것
+@AllArgsConstructor //생성자 자동 완성
+@NoArgsConstructor //생성자 자동 완성
+@EntityListeners(AuditingEntityListener.class)
+@Entity(name="meet")
+public class Meet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "meet_id")
+    private Long id;
+
+    @Column(name = "meet_name")
+    private String meetName;
+
+    @Column(name = "created_password")
+    private String createdPassword;
+
+    @Column(name = "meet_password")
+    private String meetPassword;
+
+    @CreatedDate
+    @Column(updatable = false, name = "created_date")
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "updated_date")
+    private LocalDateTime updatedDate;
+}
